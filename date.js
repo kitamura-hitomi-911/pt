ce0dae33ebad5f6e007f283d4efb2507862cef98
hitomi <hinitial@gmail.com>
2016-09-16 23:26:17 +0900

    チェック関数いろいろ

--- date.js
+++ date.js
@@ -1,15 +1,15 @@
 /**
  *
- * @param {string}current_date
- * @param {string}target_date
+ * @param target_date
+ * @param current_date
  * @returns {boolean}
  */
-function isCut2weeksToBeHeld(current_date, target_date){
+function isCut2weeksToBeHeld(target_date, current_date){
 	var is_cut = false;
-	if(current_date && target_date){
-		var current_date_obj = dateStr2dateObj(current_date);
+	if(target_date){
+		var current_date_obj = current_date ? dateStr2dateObj(current_date) : new Date();// 0時で比較するようにする
 		var target_date_obj = dateStr2dateObj(target_date);
-		if(current_date_obj && target_date_obj && target_date_obj.getTime()-current_date_obj.getTime() >= 14*24*60*60*1000){
+		if(current_date_obj && target_date_obj && current_date_obj.getTime() - target_date_obj.getTime() <= 14 * 24 * 60 * 60 * 1000){
 			is_cut = true;
 		}
 	}
@@ -26,7 +26,7 @@ function dateStr2dateObj(date_str){
 	var date_obj = new Date(date_str);
 	if(!isNaN(date_obj.getTime())){
 		var year = date_obj.getFullYear();
-		var month = date_obj.getMonth() + 1*1;
+		var month = date_obj.getMonth() + 1 * 1;
 		var date = date_obj.getDate();
 		if(date_str == year + '-' + (month < 10 ? '0' + month : month) + '-' + (date < 10 ? '0' + date : date)){
 			is_valid = date_obj;
@@ -41,8 +41,8 @@ function dateStr2dateObj(date_str){
  */
 function isDateWithinPeriod(date_str){
 	var is_within = false;
-	var start_date = new Date('2017-11-01');
-	var end_date = new Date('2017-11-30');
+	var start_date = new Date('2016-11-01');
+	var end_date = new Date('2016-11-30');
 	var date_obj = dateStr2dateObj(date_str);
 	if(date_obj && date_obj.getTime() >= start_date.getTime() && date_obj.getTime() <= end_date.getTime()){
 		is_within = true;
